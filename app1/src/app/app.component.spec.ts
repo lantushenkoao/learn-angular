5178ea205fb1d20034aaf273b085bdc7878f6888
Alexey Lantushenko <lantushenkoao@mail.ru>
2019-11-04 13:25:25 +0300

    Fixed Jasmine unit tests

--- app1/src/app/app.component.spec.ts
+++ app1/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app1 app is running!');
+    expect(compiled.querySelector('.js-heading').textContent).toContain('Testing angular syntax');
   });
 });
