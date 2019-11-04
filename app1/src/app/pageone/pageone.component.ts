8eeb829eb6a373abb0a0e3cb962a6d7cdc1e408c
Alexey Lantushenko <lantushenkoao@mail.ru>
2019-11-04 14:42:25 +0300

    Added some functionality to pageone component

--- app1/src/app/pageone/pageone.component.ts
+++ app1/src/app/pageone/pageone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from '../dao/Item';
 
 @Component({
   selector: 'app-pageone',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pageone.component.scss']
 })
 export class PageoneComponent implements OnInit {
+  items: Item[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.items = [
+      new Item('a8708fae-4d0f-4434-b480-376500e1ebb4', 'Test item 1'),
+      new Item('a18cdce2-0b30-4dc2-b933-5da79e725e88', 'Test item 2')
+    ];
   }
 
 }
+
+
+
