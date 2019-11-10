49d887f7a2a9049efe7240e81a57b445acc60e9a
Alexey Lantushenko <lantushenkoao@mail.ru>
2019-11-10 17:06:48 +0300

    Routing fixed

--- app1/src/app/app-routing.module.ts
+++ app1/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageoneComponent} from "./pageone/pageone.component";
+import {IndexComponent} from "./index/index.component";
+import {AppComponent} from "./app.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pageone', component: PageoneComponent},
+  {path: '', component: IndexComponent, pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageoneComponent } from './pageone/pageone.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageoneComponent
+    PageoneComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
